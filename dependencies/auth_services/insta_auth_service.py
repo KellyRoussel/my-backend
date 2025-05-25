23e3f9793fb947b1ed8f8667c7cd2ed01c5ba4ec
KellyRoussel <kelly.roussel@mines-ales.org>
2025-05-25 13:57:33 +0200

    feat: add debug print

--- dependencies/auth_services/insta_auth_service.py
+++ dependencies/auth_services/insta_auth_service.py
@@ -51,7 +51,7 @@ class InstaAuthService(BaseAuthService):
             auth_state = db.query(AuthState).filter(
                 AuthState.state == state,
                 AuthState.app_name == app,
-                AuthState.provider == AuthProvider.INSTAGRAM,
+                AuthState.provider == AuthProvider.INSTAGRAM.value,
                 AuthState.expires_at > datetime.utcnow()
             ).first()
 
