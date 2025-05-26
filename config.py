b72ea3e2f72f3cae7b3df7a7a9300eebffeaea1b
KellyRoussel <kelly.roussel@mines-ales.org>
2025-05-26 21:50:11 +0200

    feat: add refresh_token_expire_days in config

--- config.py
+++ config.py
@@ -19,6 +19,7 @@ class Settings(BaseSettings):
     secret_key: str = ""
     algorithm: str = ""
     access_token_expire_minutes: int = 60
+    refresh_token_expire_days: int = 30
     insta_long_lived_token_url:str = ""
     database_url: str = ""
 
